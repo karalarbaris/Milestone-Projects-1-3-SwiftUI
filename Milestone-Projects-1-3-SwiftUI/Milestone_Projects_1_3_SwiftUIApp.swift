c357b4811344a3bc017aa1cc55f10b41bfbf4050
Baris <karalarbaris@gmail.com>
2021-06-11 12:47:45 +0200

    Initial Commit

---
+++ Milestone-Projects-1-3-SwiftUI/Milestone_Projects_1_3_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Milestone_Projects_1_3_SwiftUIApp.swift
+//  Milestone-Projects-1-3-SwiftUI
+//
+//  Created by Baris Karalar on 11.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Milestone_Projects_1_3_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
