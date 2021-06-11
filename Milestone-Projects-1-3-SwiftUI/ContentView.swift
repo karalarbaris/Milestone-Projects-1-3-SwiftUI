ee0c673cf4e7287aab1465aed31ada6e3ca84a50
Baris <karalarbaris@gmail.com>
2021-06-11 17:47:35 +0200

    Challenge You have a basic understanding of arrays, state, views, images, text, and more, so let’s put them together: your challenge is to make a brain training game that challenges players to win or lose at rock, paper, scissors.
    So, very roughly:
    
    Each turn of the game the app will randomly pick either rock, paper, or scissors.
    Each turn the app will either prompt the player to win or lose.
    The player must then tap the correct move to win or lose the game.
    If they are correct they score a point; otherwise they lose a point.
    The game ends after 10 questions, at which point their score is shown.

--- Milestone-Projects-1-3-SwiftUI/ContentView.swift
+++ Milestone-Projects-1-3-SwiftUI/ContentView.swift
@@ -8,11 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var appChoice = Int.random(in: 0...2)
+    
+    @State private var playerScore = 0
+    @State private var noOfQuestionsAsked = 0
+    @State private var showingScore = false
+    
+    var possibleMoves = ["Rock", "Paper", "Scissors"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            
+            Text("Player score is: \(playerScore) ")
+  
+            Text("App's move: \(possibleMoves[appChoice])")
+            
+            ForEach(0 ..< 3) { position in
+                Button(action: {
+                    buttonTapped(position)
+
+                }, label: {
+                    Text("\(possibleMoves[position])")
+                })
+                .padding()
+                .background(Color.gray)
+                .foregroundColor(.white)
+                .clipShape(Capsule())
+            }
+            
+            
+            
+        }
+        .alert(isPresented: $showingScore, content: {
+            Alert(title: Text("Game over"), message: Text("The score is: \(playerScore)"), dismissButton: .default(Text("Start again")) {
+                noOfQuestionsAsked = 0
+                print("Game ended")
+            })
+        })
+        
+        
     }
+    
+    func buttonTapped(_ playerChoice: Int) {
+
+        if playerChoice > appChoice && playerChoice - appChoice == 1 {
+            print("player won 1")
+            playerScore += 1
+        } else if playerChoice > appChoice {
+            print("app won 2")
+            playerScore -= 1
+        } else if playerChoice < appChoice && appChoice - playerChoice == 1 {
+            print("app won 3")
+            playerScore -= 1
+        } else if playerChoice < appChoice {
+            print("player won 4")
+            playerScore += 1
+        }
+        else {
+            print("tie 5")
+        }
+        noOfQuestionsAsked += 1
+        appChoice = Int.random(in: 0...2)
+        if noOfQuestionsAsked == 10 {
+            showingScore = true
+        }
+    }
+    
 }
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
